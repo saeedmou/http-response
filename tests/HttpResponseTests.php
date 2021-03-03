0c8cdfa10f9b9c89f6a5bf9ecc516b9d7f54891f
saeedmou <saeed.mousavian@gmail.com>
2021-03-03 15:00:25 +0330

    ok

--- tests/HttpResponseTests.php
+++ tests/HttpResponseTests.php
@@ -4,7 +4,7 @@ declare (strict_types = 1);
 use Saeedmou\HttpResponse\HttpResponse;
 use PHPUnit\Framework\TestCase;
 
-class PublicInterfaceTest extends TestCase
+final class PublicInterfaceTest extends TestCase
 {
 
     public function testConstructTheNewClassInstanse(): void
