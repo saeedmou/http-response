b067478231c1220ea97caf4de2c8ed0c70a9a65f
saeedmou <saeed.mousavian@gmail.com>
2021-03-06 17:08:58 +0330

    Doubte on exit();

--- src/HttpResponse.php
+++ src/HttpResponse.php
@@ -195,7 +195,7 @@ class HttpResponse
             $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
             header('HTTP/1.1 301 Moved Permanently');
             header('Location: ' . $location);
-            exit;
+            exit();
         }
     }
 
